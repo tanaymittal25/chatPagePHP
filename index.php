c2f780a669c6a4978cf16d6473ffd654e867e1ef
Tanay Agarwal <tanay.mittal25@gmail.com>
2018-12-07 23:09:39 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <title>Chat Box</title>
+    <link rel = "stylesheet" href = "style.css" media = "all" />
+</head>
+<body>
+
+    <div class = "container">
+        <div class = "chat_box">
+            <div class = "chat_data">
+                <span class = "username"> Tanay:  </span>
+                <span class = "message"> How You Doing ? </span>
+                <span class = "time"> 22:42 </span>
+            </div>
+        </div> 
+    <form method = "POST" action = "index.php">
+    <input type = "text" name = "name" placeholder = "Name"/>
+    <textarea name = "enter message" placeholder = "Message"></textarea>
+    <input type = "submit" name = "submit" value = "Send"/>
+
+    </div>
+    
+</body>
+</html>
